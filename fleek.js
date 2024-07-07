ce6b0dd65a2e92a5f1459b87391f99f10871367d
eugenioclrc <eugenioclrc@gmail.com>
2024-07-07 10:24:42 -0300

    fixes to item drop (only drop what you can use)

--- fleek.js
+++ fleek.js
@@ -3,8 +3,7 @@ import bootstrap from './dist/index.js'
 export const main = (request) => {
     const {POST} = bootstrap(
         window ? 'https://fleek-test.network/services/1/ipfs/' + window.location.host:
-        'https://fleek-test.network/services/1/ipfs/bafkreia2wvqsew4i2cwzgqwg4fxaa2rmjo6ni4mkkez7qgp4ne2ofdj2mq');
-
+        'https://fleek-test.network/services/1/ipfs/bafybeibe7qb7bmkflttcgbxihzcuu2jafqmykzjva4pkyflmvp2k3wjy3q');
     return POST({request})
 
 };
