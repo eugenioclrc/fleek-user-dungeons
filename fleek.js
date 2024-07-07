8aa264e79d82e7856941c1674dd5bee2f5437c72
eugenioclrc <eugenioclrc@gmail.com>
2024-07-07 10:31:42 -0300

    Fix drop issues

--- fleek.js
+++ fleek.js
@@ -3,7 +3,7 @@ import bootstrap from './dist/index.js'
 export const main = (request) => {
     const {POST} = bootstrap(
         window ? 'https://fleek-test.network/services/1/ipfs/' + window.location.host:
-        'https://fleek-test.network/services/1/ipfs/bafybeibe7qb7bmkflttcgbxihzcuu2jafqmykzjva4pkyflmvp2k3wjy3q');
+        'https://fleek-test.network/services/1/ipfs/bafybeicnvecqvq3zafk7ivn4quzyo6lwybi3je4kbwecg5tqby55zd7r4e');
     return POST({request})
 
 };
